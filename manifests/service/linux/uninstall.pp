ac20c625e586d436ff38e5a98835a17174823cdc
Oji Nnorom <oji.nnorom@scotiabank.com>
2017-10-14 03:20:01 -0400

    testing

--- manifests/service/linux/uninstall.pp
+++ manifests/service/linux/uninstall.pp
@@ -26,10 +26,8 @@ define obijiautomata::service::linux::uninstall (
     cronjob: {
       exec { $title:
         path      => ['/bin','/sbin','/usr/bin','/usr/sbin','/usr/local/bin'],
-        command   => [
-          sed -i "\/.*${title}.*\/{N;d}" /var/spool/cron/crontabs/root
+        command   => "sed -i ',.*${title}.*,{N;d}' /var/spool/cron/crontabs/root",
           #sed -i "/.*${title}.*/,+1d" /var/spool/cron/crontabs/root
-        ]
       }
     }
   }
